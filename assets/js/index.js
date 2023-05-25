68d8615fbaceab5f706846cb31167036f8d29bba
ZHAN NING <112643727+la-croquette@users.noreply.github.com>
2023-05-25 09:44:02 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
 // 检测设备类型
-let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-let isSmallScreen = window.innerWidth < 1300;
+//let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+//let isSmallScreen = window.innerWidth < 1300;
 
 // 如果是手机设备或屏幕尺寸小于1300，进行跳转
-if (isMobile || isSmallScreen) {
-  window.location.href = "https://la-croquette.github.io/CercleJasmin/footer.html";
-}
+//if (isMobile || isSmallScreen) {
+  //window.location.href = "https://la-croquette.github.io/CercleJasmin/footer.html";
+//}
 
 window.onload = function() {
   fetch('header.html')
